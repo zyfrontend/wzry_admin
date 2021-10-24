955be8c757d32b92bf8839ab4183d31edeba1a10
zy <7463910+zyfullstack@user.noreply.gitee.com>
2021-10-24 14:55:42 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './plugins/element.js'
+import http from './network/http'
+import './style.css'
+
+Vue.config.productionTip = false
+Vue.prototype.$http = http
+Vue.mixin({
+  computed: {
+    uploadUrl(){
+      return this.$http.defaults.baseURL+ '/upload'
+    }
+  },
+  methods: {
+    getAuthHeaders(){
+      return {
+        Authorization: `Bearer ${localStorage.token || ''}`
+      }
+    }
+  }
+})
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
